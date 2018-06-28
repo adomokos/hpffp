ce7942995df0fea6be1e3ca2fed6085d1b7f66e3
Attila Domokos <adomokos@gmail.com>
2018-06-27 22:00:48 -0500

    Work on the exercises

--- test/Hpffp/QuickCheckSpec.hs
+++ test/Hpffp/QuickCheckSpec.hs
@@ -3,6 +3,7 @@ module Hpffp.QuickCheckSpec where
 import Test.Hspec
 import Test.QuickCheck
 import Data.Numbers.Primes
+import Data.List (sort)
 
 half :: (Fractional a) => a -> a
 half x = x / 2
@@ -45,12 +46,60 @@ prop_PrimeSum_v4 :: Prime Int -> Prime Int -> Property
 prop_PrimeSum_v4 (Prime p) (Prime q) =
     p > 2 && q > 2 ==> classify (p < 1000 || q < 1000) "has small prime" $ even (p + q)
 
+-- Sorting
+
+genList :: (Arbitrary a, Eq a) => Gen [a]
+genList = do
+  a <- arbitrary
+  b <- arbitrary `suchThat` (/= a)
+  c <- arbitrary `suchThat` (`notElem` [a, b])
+  return [a, b, c]
+
+listOrdered :: (Ord a) => [a] -> Bool
+listOrdered xs =
+    snd $ foldr go (Nothing, True) xs
+        where go _ status@(_, False) = status
+              go y (Nothing, t) = (Just y, t)
+              go y (Just x, t) = (Just y, x >= y)
+
+prop_listOrdered :: Property
+prop_listOrdered =
+    forAll (genList :: Gen String)
+    (\x -> listOrdered $ sort x)
+
+-- Addition
+
+associative :: (Num a, Eq a) => a -> a -> a -> Bool
+associative x y z = x + (y + z) == (x + y) + z
+
+commutative :: (Num a, Eq a) => a -> a -> Bool
+commutative x y = x + y == y + x
+
+genTuple :: (Arbitrary a, Arbitrary b) => Gen (a, b)
+genTuple = arbitrary
+
+genThreeple :: (Arbitrary a, Arbitrary b, Arbitrary c) => Gen (a, b, c)
+genThreeple = arbitrary
+
+prop_plusAssoc :: Property
+prop_plusAssoc =
+    forAll (genThreeple :: Gen (Int, Int, Int))
+    (\(x, y, z) -> associative x y z)
+
+prop_plusCommutative :: Property
+prop_plusCommutative =
+    forAll (genTuple :: Gen (Int, Int))
+    (\(x, y) -> commutative x y)
+
+
 spec :: Spec
 spec = do
     describe "Learning QuickCheck" $ do
         it "verifies half function works" $ property $ do
             \x -> ((*2) . half) x == (x :: Rational)
-        it "can reverse and reverse list" $
-            property prop_RevRev
+        it "can reverse and reverse list" $ property prop_RevRev
         it "can verify a property of a list" $ property prop_Index_v3
+        it "verifies an ordered list" $ property prop_listOrdered
+        it "verifies associative function" $ property prop_plusAssoc
+        it "verifies associative function" $ property prop_plusCommutative
         -- it "can verify prime numbers" $ property prop_PrimeSum_v4
