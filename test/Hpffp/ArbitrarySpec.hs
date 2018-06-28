e1ac214ef04c1ea1aecb59170fe8d134e0b26ccf
Attila Domokos <adomokos@gmail.com>
2018-06-27 19:11:44 -0500

    Learn about Arbitrary and Coarbitrary

--- test/Hpffp/ArbitrarySpec.hs
+++ test/Hpffp/ArbitrarySpec.hs
@@ -36,6 +36,59 @@ identityGenInt = identityGen
     {- sample trivialGen -}
     {- sample identityGenInt -}
 
+data Pair a b =
+    Pair a b deriving Show
+
+pairGen :: (Arbitrary a,
+            Arbitrary b) => Gen (Pair a b)
+pairGen = do
+    a <- arbitrary
+    b <- arbitrary
+    return (Pair a b)
+
+instance (Arbitrary a,
+          Arbitrary b) =>
+          Arbitrary (Pair a b) where
+    arbitrary = pairGen
+
+pairGenIntString :: Gen (Pair Int String)
+pairGenIntString = pairGen
+
+data Sum a b = First a
+             | Second b
+             deriving (Eq, Show)
+
+-- equal odds for each
+sumGenEqual :: (Arbitrary a,
+                Arbitrary b) =>
+                Gen (Sum a b)
+sumGenEqual = do
+    a <- arbitrary
+    b <- arbitrary
+    oneof [return $ First a,
+           return $ Second b]
+
+sumGenFirstPls :: (Arbitrary a,
+                   Arbitrary b) =>
+                   Gen (Sum a b)
+sumGenFirstPls = do
+    a <- arbitrary
+    b <- arbitrary
+    frequency [(10, return $ First a),
+               (1, return $ Second b)]
+
+sumGenCharInt :: Gen (Sum Char Int)
+sumGenCharInt = sumGenEqual
+
+sumGenCharIntFirst :: Gen (Sum Char Int)
+sumGenCharIntFirst = sumGenFirstPls
+
+{- instance Arbitrary a => -}
+         {- Arbitrary (Maybe a) where -}
+    {- arbitrary = -}
+        {- frequency [(1, return Nothing), -}
+                   {- (3, liftM Just arbitrary)] -}
+
 spec :: Spec
 spec = do
     describe "Arbitrary" $ do
