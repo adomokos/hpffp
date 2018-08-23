0a47d09730391d42abf2e7b462c1dfa4b1276bd8
Attila Domokos <adomokos@gmail.com>
2018-08-23 09:27:58 -0500

    Learn more about the Reader

--- test/Hpffp/ReaderSpec.hs
+++ test/Hpffp/ReaderSpec.hs
@@ -61,6 +61,65 @@ tupled'' = do
 tupled''' :: [Char] -> ([Char], [Char])
 tupled''' = (,) =<< rev <$> cap
 
+{-
+    (.) :: (b -> c) -> (a -> b) -> a -> c
+    -- or perhaps
+    (.) :: (b -> c) -> (a -> b) -> (a -> c)
+
+    -- How does this line up with Functor?
+    (.) :: (b -> c) -> (a -> b) -> (a -> c)
+    fmap :: Functor f => (a -> b) -> f a -> f b
+
+    -- Remove the name of the functions
+
+    :: (b -> c) -> (a -> b) -> (a -> c)
+    :: (a -> b) ->     f a  ->     f b
+
+    -- Changing up the letters
+    -- Without changing the meaning
+
+    :: (b -> c) -> (a -> b) -> (a -> c)
+    :: (b -> c) ->     f b  ->     f c
+
+    -- f is ((->) a)
+    :: (b -> c) -> (a    -> b) -> (a   -> c)
+    :: (b -> c) -> ((->) a) b -> ((->) a) c
+
+    -- Unroll the prefix notation into infix
+
+    :: (b -> c) -> (a -> b) -> (a -> c)
+    :: (b -> c) -> (a -> b) -> (a -> c)
+
+    -- Bada ding. Functorial lifting for functions.
+-}
+
+newtype Reader r a =
+    Reader { runReader :: r -> a }
+
+instance Functor (Reader r) where
+    {- fmap :: (a -> b) -}
+         {- -> Reader r a -}
+         {- -> Reader r b -}
+    fmap f (Reader ra) = Reader $ \r -> f (ra r)
+
+-- same as (.)
+compose :: (b -> c) -> (a -> b) -> (a -> c)
+compose f g = \x -> f (g x)
+
+{-
+    See it?
+    \r -> f (ra r)
+    \x -> f (g  x)
+
+    We can use the fact that this is function composition.
+
+instance Functor (Reader r) where
+    fmap f (Reader ra) = Reader $ (f . ra)
+-}
+
+ask :: Reader a a
+ask = Reader id
+
 spec :: Spec
 spec = do
     describe "Functions" $ do
@@ -79,3 +138,13 @@ spec = do
             tupled' "Julie" `shouldBe` ("eiluJ", "JULIE")
             tupled'' "Julie" `shouldBe` ("eiluJ", "JULIE")
             tupled''' "Julie" `shouldBe` ("EILUJ", "Julie")
+    describe "Functor class" $ do
+        it "has function implementation" $ do
+            -- instance Functor ((->) r) where
+            --     fmap = (.)
+            fmap (+1) (*2) 3 `shouldBe` 7
+            (fmap (+1) (*2)) 3 `shouldBe` 7
+            ((+1) . (*2)) 3 `shouldBe` 7
+            ((+2) . (*1)) 2 `shouldBe` 4
+        it "can work with a custom ask function" $ do
+            ((runReader ask) (+2)) 3 `shouldBe` 5
