c28bf56577cd821310c6e696e42904dc6a3f858c
Attila Domokos <adomokos@gmail.com>
2018-09-05 20:34:48 -0500

    Learn about the MaybeT monad transformer

--- test/Hpffp/MonadTransformersSpec.hs
+++ test/Hpffp/MonadTransformersSpec.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE InstanceSigs, GeneralizedNewtypeDeriving #-}
 module Hpffp.MonadTransformersSpec where
 
 import Test.Hspec
@@ -20,12 +20,33 @@ instance (Applicative m)
     (MaybeT fab) <*> (MaybeT mma) =
         MaybeT $ (<*>) <$> fab <*> mma
 
+instance (Monad m)
+        => Monad (MaybeT m) where
+    return = pure
+    (>>=) :: MaybeT m a
+          -> (a -> MaybeT m b)
+          -> MaybeT m b
+    (MaybeT ma) >>= f =
+        MaybeT $ do
+            -- ma :: m (Maybe a)
+            -- v :: Maybe a
+            v <- ma
+            case v of
+                Nothing -> return Nothing
+                Just y -> runMaybeT (f y)
+
 spec :: Spec
 spec = do
     describe "Monad transformers" $ do
-        it "can work with MaybeT" $ do
+        it "can work with MaybeT Functors" $ do
             let x = MaybeT $ Just (Just 2)
             runMaybeT (fmap (+1) x) `shouldBe` Just (Just 3)
             let y = MaybeT $ [Just "No",Nothing]
             runMaybeT (fmap (++"!") y)
                 `shouldBe` [Just "No!",Nothing]
+        it "can work with MaybeT Monads" $ do
+            let x = MaybeT $ [(Just 2), Nothing]
+            -- (map . fmap) (*2) [Just 2, Nothing]
+            pending
+
+
