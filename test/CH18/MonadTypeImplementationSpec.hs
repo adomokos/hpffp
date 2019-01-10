c22989f60dff6237b78248e3c179899d6a801601
Attila Domokos <adomokos@gmail.com>
2019-01-10 08:00:19 -0600

    My implementation of Either with Monad

--- test/CH18/MonadTypeImplementationSpec.hs
+++ test/CH18/MonadTypeImplementationSpec.hs
@@ -25,6 +25,26 @@ instance Monad MyMaybe where
   MyNothing >>= _ = MyNothing
   MyJust x >>= f = f x
 
+data MyEither a b
+  = MyLeft a
+  | MyRight b
+  deriving (Show, Eq)
+
+instance Functor (MyEither a) where
+  fmap _ (MyLeft x) = MyLeft x
+  fmap f (MyRight y) = MyRight (f y)
+
+instance Applicative (MyEither a) where
+  pure x = MyRight x
+  MyLeft x <*> _ = MyLeft x
+  _ <*> MyLeft x = MyLeft x
+  MyRight f <*> MyRight y = MyRight (f y)
+
+instance Monad (MyEither a) where
+  return = pure
+  MyLeft x >>= _ = MyLeft x
+  MyRight x >>= f = f x
+
 spec :: Spec
 spec = do
   describe "Monads for my Maybe" $ do
@@ -37,3 +57,19 @@ spec = do
       (*) <$> value <*> MyNothing `shouldBe` MyNothing
     it "passes for Monads" $ do
       ((pure 2 >>= (\x -> MyJust (x * 2))) :: MyMaybe Int) `shouldBe` MyJust 4
+  describe "Monads for my Either" $ do
+    it "passes for Functor" $ do
+      (((*2) <$> MyRight 3) :: MyEither String Int) `shouldBe` MyRight 6
+      (((*2) <$> MyLeft "Ho") :: MyEither String Int) `shouldBe` MyLeft "Ho"
+    it "passes for Applicative" $ do
+      (((*) <$> MyRight 2 <*> MyRight 3) :: MyEither String Int)
+        `shouldBe` MyRight 6
+      (((*) <$> MyRight 2 <*> MyLeft "Ho") :: MyEither String Int)
+        `shouldBe` MyLeft "Ho"
+      (((*) <$> MyLeft "Ho" <*> MyRight 2) :: MyEither String Int)
+        `shouldBe` MyLeft "Ho"
+    it "passes for Monad" $ do
+      ((pure 3 >>= (\x -> MyRight (x*2))) :: MyEither String Int)
+        `shouldBe` MyRight 6
+      ((MyLeft "Ho" >>= (\x -> MyRight (x*2))) :: MyEither String Int)
+        `shouldBe` MyLeft "Ho"
