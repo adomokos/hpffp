173dfda29bd5d7c4dde895aa0b7de35ac9ffa015
Attila Domokos <adomokos@gmail.com>
2019-02-23 11:58:37 -0600

    More intro to Foldable

--- test/CH20/FoldableIntroSpec.hs
+++ test/CH20/FoldableIntroSpec.hs
@@ -3,7 +3,8 @@ module CH20.FoldableIntroSpec where
 
 import Test.Hspec
 import Data.Foldable
-import Data.Monoid (Sum(..))
+import Data.Monoid (Sum(..), Product(..), All(..), Any(..)
+                   ,First(..), Last(..))
 
 newtype MySum = MySum Int deriving (Eq, Show)
 
@@ -16,10 +17,20 @@ instance Monoid MySum where
     fold :: Monoid m => t m -> m
     foldMap :: Monoid m
             => (a -> m) -> t a -> m
+
+  Foldable is a way of generalizing catamorphisms - folding -
+  to different datatypes, it forces you to think about the monoid
+  you're using to combine values.
 -}
 
 main :: IO ()
 main = hspec spec
+
+xs :: [Sum Integer]
+xs = [1,2,3,4,5]
+
+ys :: [Product Integer]
+ys = [1,2,3,4,5]
 
 spec :: Spec
 spec =
@@ -30,5 +41,48 @@ spec =
     it "can be observed in a very basic operation" $ do
       foldr (+) 0 ([1..5] :: [Int]) `shouldBe` 15
       sum ([1..5] :: [Int]) `shouldBe` 15
-    it "can fold over Monoid lists" $ do
-      fold (map Sum [1..5])`shouldBe` Sum 15
+
+    context "fold" $ do
+      it "can fold over Monoid lists" $ do
+        fold (map Sum [1..5]) `shouldBe` Sum 15
+        fold xs `shouldBe` Sum 15
+        fold ys `shouldBe` Product 120
+        fold ["hello", "world"] `shouldBe` "helloworld"
+
+    context "foldMap" $ do
+      it "uses a function in foldMap" $ do
+        foldMap Sum [1,2,3,4] `shouldBe` Sum 10
+        foldMap Product [1,2,3,4] `shouldBe` Product 24
+        foldMap All [True, False, True] `shouldBe` All False
+        foldMap Any [(3==4), (4<5)] `shouldBe` Any True
+        foldMap First [Just 1, Nothing, Just 5]
+          `shouldBe` First (Just 1)
+        foldMap Last [Just 1, Nothing, Just 5]
+          `shouldBe` Last (Just 5)
+      it "recognizes foldMap with a Monoid" $
+        foldMap MySum [1,2,3,4] `shouldBe` MySum 10
+      it "can use regular functions with it" $ do
+        let products = map Product [1..3]
+        foldMap (*5) products `shouldBe` Product 750
+        let sums = map Sum [1..3]
+        foldMap (*5) sums `shouldBe` Sum 30
+      it "will not change the foldMap behavior with 1 instance" $ do
+        let result = foldMap (*5) (Just 100) :: Product Integer
+            result2 = foldMap (*5) (Just 5) :: Sum Integer
+        result `shouldBe` 500
+        result2 `shouldBe` Sum 25
+      it "uses mempty when the fold subject is empty" $ do
+        let result = foldMap (*5) Nothing :: Sum Integer
+            result2 = foldMap (*5) Nothing :: Product Integer
+        result `shouldBe` Sum 0
+        result2 `shouldBe` Product 1
+
+    context "foldr" $ do
+      it "has the Monoid instance baked in" $ do
+        foldr (*) 5 [1,2,3] `shouldBe` 30
+      it "ignores the Monoid instance" $ do
+        let sumXs = map Sum [2..4]
+        foldr (*) 3 sumXs `shouldBe` Sum 72
+        let productXs = map Product [2..4]
+        foldr (*) 3 productXs `shouldBe` Product 72
+
